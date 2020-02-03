13150fbf887d930961396ff5f256d242bbc70a0d
Kai Lehmann <kai.uni-assist@lekatech.de>
2020-02-03 11:55:04 +0100

    Implement settings and ping REST API endpoints

---
+++ src/main/java/de/aservo/atlassian/bitbucket/confapi/model/SettingsBean.java
@@ -0,0 +1,69 @@
+package de.aservo.atlassian.bitbucket.confapi.model;
+
+import com.atlassian.bitbucket.server.ApplicationMode;
+import com.atlassian.bitbucket.server.ApplicationPropertiesService;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.net.URI;
+import java.util.Arrays;
+
+@XmlRootElement(name = "settings")
+public class SettingsBean {
+
+    @XmlElement
+    private final String baseurl;
+
+    @XmlElement
+    private final String mode;
+
+    @XmlElement
+    private final String title;
+
+    /**
+     * The default constructor is needed for JSON request deserialization.
+     */
+    public SettingsBean() {
+        this.baseurl = null;
+        this.mode = null;
+        this.title = null;
+    }
+
+    public SettingsBean(
+            final String baseUrl,
+            final String mode,
+            final String title) {
+
+        this.baseurl = baseUrl;
+        this.mode = mode != null ? mode.toLowerCase() : null;
+        this.title = title;
+    }
+
+    public URI getBaseurl() {
+        return baseurl != null ? URI.create(baseurl) : null;
+    }
+
+    public ApplicationMode getMode() {
+        return Arrays.stream(ApplicationMode.values())
+                .filter(m -> m.getId().equalsIgnoreCase(mode))
+                .findAny().orElse(null);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public static SettingsBean from(
+            final ApplicationPropertiesService applicationPropertiesService) {
+
+        final String baseurl = applicationPropertiesService.getBaseUrl() != null ?
+                applicationPropertiesService.getBaseUrl().toString() : null;
+
+        return new SettingsBean(
+                baseurl,
+                applicationPropertiesService.getMode().toString(),
+                applicationPropertiesService.getDisplayName()
+        );
+    }
+
+}
---
+++ src/test/java/de/aservo/atlassian/bitbucket/confapi/rest/SettingsResourceTest.java
@@ -0,0 +1,67 @@
+package de.aservo.atlassian.bitbucket.confapi.rest;
+
+import com.atlassian.bitbucket.server.ApplicationMode;
+import com.atlassian.bitbucket.server.ApplicationPropertiesService;
+import de.aservo.atlassian.bitbucket.confapi.model.SettingsBean;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.ws.rs.core.Response;
+import java.net.URI;
+
+import static de.aservo.atlassian.bitbucket.confapi.model.SettingsBeanTest.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SettingsResourceTest {
+
+    @Mock
+    private ApplicationPropertiesService applicationPropertiesService;
+
+    private SettingsResource settingsResource;
+
+    @Before
+    public void setup() {
+        when(applicationPropertiesService.getBaseUrl()).thenReturn(URI.create(BASEURL));
+        when(applicationPropertiesService.getMode()).thenReturn(ApplicationMode.valueOf(MODE));
+        when(applicationPropertiesService.getDisplayName()).thenReturn(TITLE);
+
+        settingsResource = new SettingsResource(applicationPropertiesService);
+    }
+
+    @Test
+    public void testGetSettings() {
+        final Response response = settingsResource.getSettings();
+        final SettingsBean bean = (SettingsBean) response.getEntity();
+
+        assertEquals(applicationPropertiesService.getBaseUrl(), bean.getBaseurl());
+        assertEquals(applicationPropertiesService.getMode(), bean.getMode());
+        assertEquals(applicationPropertiesService.getDisplayName(), bean.getTitle());
+    }
+
+    @Test
+    public void testSetSettings() {
+        final SettingsBean settingsBean = SettingsBean.from(applicationPropertiesService);
+        final Response response = settingsResource.putSettings(settingsBean);
+
+        assertNotNull(applicationPropertiesService.getBaseUrl());
+        assertNotNull(applicationPropertiesService.getDisplayName());
+
+        ArgumentCaptor<URI> baseurlCaptor = ArgumentCaptor.forClass(URI.class);
+        ArgumentCaptor<String> titleCaptor = ArgumentCaptor.forClass(String.class);
+
+        verify(applicationPropertiesService).setBaseURL(baseurlCaptor.capture());
+        verify(applicationPropertiesService).setDisplayName(titleCaptor.capture());
+
+        assertEquals(applicationPropertiesService.getBaseUrl(), baseurlCaptor.getValue());
+        assertEquals(applicationPropertiesService.getDisplayName(), titleCaptor.getValue());
+    }
+
+}
